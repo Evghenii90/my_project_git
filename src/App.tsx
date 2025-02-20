3b7e2f311a8a7abe53f0e8ae17fba3557de5b47a
Evghenii90 <litvinenkoevgeniy3359@gmail.com>
2025-02-20 16:53:32 +0200

    Dev branch commit 1

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>Lorem ipsum dolor sit amet.</p>
-        <p>2.</p>
-        <p>3.</p>
-      </header>
+    <div >
+      <div>App</div>
+      <div>Dev branch commit 1</div>
     </div>
   );
 }
